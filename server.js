bae90ce2b237f261b6a137233d9b0777bf95fee0
Anik Zod <anikdas169@gmail.com>
2026-02-20 02:25:36 +0600

    ogaglow admin url put

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const limiter = rateLimit({
 // app.use(limiter);
 
 // CORS Middleware (before any route)
-const allowedOrigins = [process.env.FRONTEND_URL, process.env.ADMIN_URL, process.env.FRONTEND_URL_LOCAL, process.env.ADMIN_URL_LOCAL, process.env.OGAGLOW_URL].filter(Boolean);
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.ADMIN_URL, process.env.FRONTEND_URL_LOCAL, process.env.ADMIN_URL_LOCAL, process.env.OGAGLOW_URL, process.env.OGAGLOW_ADMIN_URL].filter(Boolean);
 app.use(
   cors({
     origin: allowedOrigins.length ? allowedOrigins : false,
