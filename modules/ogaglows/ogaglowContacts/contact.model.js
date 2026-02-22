758b16817ec5560e2ecd192de6036687b063c2da
Anik Zod <anikdas169@gmail.com>
2026-02-22 23:31:54 +0600

    feat: add Mongoose model for contact form submissions.

--- modules/ogaglows/ogaglowContacts/contact.model.js
+++ modules/ogaglows/ogaglowContacts/contact.model.js
@@ -11,7 +11,6 @@ const contactSchema = new mongoose.Schema(
         email: {
             type: String,
             required: [true, "Please enter email"],
-            unique: true,
             lowercase: true,
             trim: true,
         },
