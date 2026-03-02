36dab19aba7b8061f0bbe8c4bb5b25ff66c2421c
Anik Zod <anikdas169@gmail.com>
2026-03-02 21:40:56 +0600

    dsa

--- modules/ogaglows/orders/order.controller.js
+++ modules/ogaglows/orders/order.controller.js
@@ -8,7 +8,7 @@ const round = (num) => Math.round(num * 100) / 100;
 
 // price calculation 
 export const checkoutPreview = async (req, res) => {
-  const result = await checkoutPreviewService({ ...req.body, userId: req.user.id });
+  const result = await checkoutPreviewService({ ...req.body });
 
   res.status(200).json({
     success: true,
